1bde824944cb598ce6d0e2f219ff9ee569d08c18
fredymarb <29468132+fredymarb@users.noreply.github.com>
2024-09-25 11:21:01 +0000

    feat: Board#winner?
    desc: checks the board for a winning combination
      and returns a boolean

--- lib/board.rb
+++ lib/board.rb
@@ -44,4 +44,38 @@ class Board
   def board_full?
     @grid.flatten.none?(EMPTY_SLOT)
   end
+
+  def winner?
+    horizontal_win? || vertical_win? || diagonal_win?
+  end
+
+  private
+
+  def horizontal_win?
+    @grid.any? do |row|
+      row.each_cons(4).any? { |cons| cons.uniq == [@current_player.token] }
+    end
+  end
+
+  def vertical_win?
+    @grid.transpose.any? do |col|
+      col.each_cons(4).any? { |cons| cons.uniq == [@current_player.token] }
+    end
+  end
+
+  def diagonal_win?
+    diagonal_check?(@grid) || diagonal_check?(@grid.map(&:reverse))
+  end
+
+  def diagonal_check?(board)
+    offset = [0, 1, 2, 3]
+
+    (0..ROWS - offset.size).each do |row|
+      (0..COLUMNS - offset.size).each do |col|
+        return true if offset.all? { |i| board[row + i][col + i] == @current_player.token }
+      end
+    end
+
+    false
+  end
 end
